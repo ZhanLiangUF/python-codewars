25c59bafe89201f53d635a24e4734bd0d8d2e7e7
ZhanLiangUF <zliang@ufl.edu>
2017-01-30 20:14:57 -0500

    Add optimal solution

--- Isograms.py
+++ Isograms.py
@@ -1,4 +1,4 @@
-# An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that 
+# An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that
 # contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
 
 def is_isogram(string):
@@ -10,3 +10,8 @@ def is_isogram(string):
         else:
             return False
     return True
+
+
+# Below is optimal solution, which uses a set which gets rid of duplicates and if the string is an isogram, it should be same length
+def is_isogram_2(string):
+    return len(string) == len(set(string.lower()))
